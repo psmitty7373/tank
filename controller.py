e38b3e12307df92638f1a604e14b5383f620a3f5
psmitty7373 <phillip@ironrain.org>
2019-02-24 19:58:48 +0000

    - tweaks

--- controller.py
+++ controller.py
@@ -14,40 +14,38 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     return rightMin + (valueScaled * rightSpan)
 
 def steering(x, y, minJoystick, maxJoystick, minSpeed, maxSpeed):
-	if x == 0 and y == 0:
-		return (0, 0)
+    if x == 0 and y == 0:
+        return (0, 0)
 
-	z = math.sqrt(x * x + y * y)
-	rad = math.acos(math.fabs(x) / z)
-	angle = rad * 180 / math.pi
-	tcoeff = -1 + (angle / 90) * 2
-	turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
-	turn = round(turn * 100, 0) / 100
-	mov = max(math.fabs(y), math.fabs(x))
+    z = math.sqrt(x * x + y * y)
+    rad = math.acos(math.fabs(x) / z)
+    angle = rad * 180 / math.pi
+    tcoeff = -1 + (angle / 90) * 2
+    turn = tcoeff * math.fabs(math.fabs(y) - math.fabs(x))
+    turn = round(turn * 100, 0) / 100
+    mov = max(math.fabs(y), math.fabs(x))
 
-	if (x >= 0 and y >= 0) or (x < 0 and y < 0):
-		rawLeft = mov
-		rawRight = turn
-	else:
-		rawRight = mov
-		rawLeft = turn
+    if (x >= 0 and y >= 0) or (x < 0 and y < 0):
+        rawLeft = mov
+        rawRight = turn
+    else:
+        rawRight = mov
+        rawLeft = turn
 
-	if y < 0:
-		rawLeft = 0 - rawLeft
-		rawRight = 0 - rawRight
+    if y < 0:
+        rawLeft = 0 - rawLeft
+        rawRight = 0 - rawRight
 
-	rightOut = translate(rawRight, minJoystick, maxJoystick, minSpeed, maxSpeed)
-	leftOut = translate(rawLeft, minJoystick, maxJoystick, minSpeed, maxSpeed)
+    rightOut = translate(rawRight, minJoystick, maxJoystick, minSpeed, maxSpeed)
+    leftOut = translate(rawLeft, minJoystick, maxJoystick, minSpeed, maxSpeed)
 
-	return (round(rightOut), round(leftOut))
+    return (round(rightOut), round(leftOut))
 
-right_pwm = 10
-right_s1 = 27
-right_s2 = 22
+right_pwm_f = 27
+right_pwm_r = 17
 
-left_pwm = 17 
-left_s1 = 9 
-left_s2 = 11
+left_pwm_f = 23
+left_pwm_r = 24
 
 serial_port_enabled = False
 serial_port_mode = "UWB"
@@ -83,21 +81,15 @@ class Tank(multiprocessing.Process):
             self.bno = None
 
         #right
-        self.pi.set_mode(right_pwm, pigpio.OUTPUT)
-        self.pi.set_mode(right_s1, pigpio.OUTPUT)
-        self.pi.set_mode(right_s2, pigpio.OUTPUT)
-        self.pi.write(right_s1, 0)
-        self.pi.write(right_s2, 1)
+        self.pi.set_mode(right_pwm_f, pigpio.OUTPUT)
+        self.pi.set_mode(right_pwm_r, pigpio.OUTPUT)
         self.right_dir = 'f'
         self.right_braking = False
         self.right_brake_time = time.time() * 1000
 
         #left
-        self.pi.set_mode(left_pwm, pigpio.OUTPUT)
-        self.pi.set_mode(left_s1, pigpio.OUTPUT)
-        self.pi.set_mode(left_s2, pigpio.OUTPUT)
-        self.pi.write(left_s1, 1)
-        self.pi.write(left_s2, 0)
+        self.pi.set_mode(left_pwm_f, pigpio.OUTPUT)
+        self.pi.set_mode(left_pwm_r, pigpio.OUTPUT)
         self.left_dir = 'f'
         self.left_braking = False
         self.left_brake_time = time.time() * 1000
@@ -184,8 +176,10 @@ class Tank(multiprocessing.Process):
 
             # if no update in a second, stop motors
             if curr_time - self.failsafe_time > 1000:
-                self.pi.set_servo_pulsewidth(left_pwm, 0)
-                self.pi.set_servo_pulsewidth(right_pwm, 0)
+                self.pi.set_PWM_dutycycle(left_pwm_f, 0)
+                self.pi.set_PWM_dutycycle(left_pwm_r, 0)
+                self.pi.set_PWM_dutycycle(right_pwm_f, 0)
+                self.pi.set_PWM_dutycycle(right_pwm_r, 0)
 
             # UWB
             # poll uwb serial port
@@ -200,7 +194,6 @@ class Tank(multiprocessing.Process):
                 self.ppos_x, self.ppos_y, self.ppos_z = self.pf.getEstimate()
                 if self.imu_enabled and self.bno:
                     self.azim_x, self.azim_y, self.azim_z = self.bno.getVector(BNO055.BNO055.VECTOR_EULER)
-
 
             while not self.tasks.empty():
                 self.failsafe_time = curr_time
@@ -218,21 +211,15 @@ class Tank(multiprocessing.Process):
                         if not self.right_braking:
                             self.right_braking = True
                             self.right_brake_time = time.time() * 1000
-                            self.pi.write(right_s1, 0)
-                            self.pi.write(right_s2, 0)
 
                         # done braking
                         if time.time() * 1000 - self.right_brake_time > self.brake_time:
                             self.right_braking = False
-                            self.pi.write(right_s1, 0)
-                            self.pi.write(right_s2, 1)
                             self.right_dir = 'f'
 
                     # same dir, cancel braking
                     elif self.right_braking:
                         self.right_braking = False
-                        self.pi.write(right_s1, 0)
-                        self.pi.write(right_s2, 1)
 
                 elif r < 0:
                     if self.right_dir == 'f':
@@ -240,21 +227,15 @@ class Tank(multiprocessing.Process):
                         if not self.right_braking:
                             self.right_braking = True
                             self.right_brake_time = time.time() * 1000
-                            self.pi.write(right_s1, 0)
-                            self.pi.write(right_s2, 0)
 
                         # done braking
                         if time.time() * 1000 - self.right_brake_time > self.brake_time:
                             self.right_braking = False
-                            self.pi.write(right_s1, 1)
-                            self.pi.write(right_s2, 0)
                             self.right_dir = 'b'
 
                     # same dir, cancel braking
                     elif self.right_braking:
                         self.right_braking = False
-                        self.pi.write(right_s1, 1)
-                        self.pi.write(right_s2, 0)
 
                 # left motor
                 if l > 0: # forward
@@ -264,21 +245,15 @@ class Tank(multiprocessing.Process):
                         if not self.left_braking:
                             self.left_braking = True
                             self.left_brake_time = time.time() * 1000
-                            self.pi.write(left_s1, 0)
-                            self.pi.write(left_s2, 0)
 
                         # done braking
                         if time.time() * 1000 - self.left_brake_time > self.brake_time:
                             self.left_braking = False
-                            self.pi.write(left_s1, 1)
-                            self.pi.write(left_s2, 0)
                             self.left_dir = 'f'
 
                     # same dir, cancel braking
                     elif self.left_braking:
                         self.left_braking = False
-                        self.pi.write(left_s1, 1)
-                        self.pi.write(left_s2, 0)
 
                 elif l < 0:
                     if self.left_dir == 'f':
@@ -286,31 +261,37 @@ class Tank(multiprocessing.Process):
                         if not self.left_braking:
                             self.left_braking = True
                             self.left_brake_time = time.time() * 1000
-                            self.pi.write(left_s1, 0)
-                            self.pi.write(left_s2, 0)
 
                         # done braking
                         if time.time() * 1000 - self.left_brake_time > self.brake_time:
                             self.left_braking = False
-                            self.pi.write(left_s1, 0)
-                            self.pi.write(left_s2, 1)
                             self.left_dir = 'b'
 
                     # same dir, cancel braking
                     elif self.left_braking:
                         self.left_braking = False
-                        self.pi.write(left_s1, 0)
-                        self.pi.write(left_s2, 1)
-
-                if r == 0 or self.right_braking:
-                    self.pi.set_servo_pulsewidth(left_pwm, 0)
-                else:
-                    self.pi.set_servo_pulsewidth(left_pwm, translate(abs(r), 0, 100, 500, 2000))
 
                 if l == 0 or self.left_braking:
-                    self.pi.set_servo_pulsewidth(right_pwm, 0)
+                    self.pi.set_PWM_dutycycle(left_pwm_f, 0)
+                    self.pi.set_PWM_dutycycle(left_pwm_r, 0)
                 else:
-                    self.pi.set_servo_pulsewidth(right_pwm, translate(abs(l), 0, 100, 500, 2000))
+                    if self.left_dir == 'f':
+                        self.pi.set_PWM_dutycycle(left_pwm_r, 0)
+                        self.pi.set_PWM_dutycycle(left_pwm_f, abs(l))
+                    else:
+                        self.pi.set_PWM_dutycycle(left_pwm_f, 0)
+                        self.pi.set_PWM_dutycycle(left_pwm_r, abs(l))
+
+                if r == 0 or self.right_braking:
+                    self.pi.set_PWM_dutycycle(right_pwm_f, 0)
+                    self.pi.set_PWM_dutycycle(right_pwm_r, 0)
+                else:
+                    if self.right_dir == 'f':
+                        self.pi.set_PWM_dutycycle(right_pwm_r, 0)
+                        self.pi.set_PWM_dutycycle(right_pwm_f, abs(r))
+                    else:
+                        self.pi.set_PWM_dutycycle(right_pwm_f, 0)
+                        self.pi.set_PWM_dutycycle(right_pwm_r, abs(r))
 
                 #semd status back
                 if not self.results.full() and time.time() * 1000 - self.heartbeat_time > 200:
@@ -344,7 +325,7 @@ class WebSocket(tornado.websocket.WebSocketHandler):
  
     def on_message(self, message):
         pos = json.loads(message)
-        r, l = steering(pos['x'], pos['y'], -100, 100, -100, 100)
+        r, l = steering(pos['x'], pos['y'], -100, 100, -255, 255)
         self.tasks.put({'task_type': 'throttle_update', 'payload': (r,l)})
         while not self.results.empty():
             msg = self.results.get()
