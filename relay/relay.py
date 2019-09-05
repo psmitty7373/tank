51c74ee8809cb7afb0517b4599e309346c6fc36d
psmitty7373 <psmitty7373@gmail.com>
2019-09-05 12:39:02 +0100

    test

--- relay/relay.py
+++ relay/relay.py
@@ -158,6 +158,7 @@ class SocketThread(Thread):
                             self.pipe[1].send({'s': c, 'sport': self.sport, 'eport': addr[1], 'cmd': NEW, 'master': True, 'payload': b'\0'})
                             self.pipe[1].send({'sport': self.sport, 'eport': addr[1], 'cmd': READY, 'payload': b'\0'})
                         else:
+                            print('kill')
                             c.close()
 
                     else:
@@ -165,8 +166,6 @@ class SocketThread(Thread):
                             self.pipe[1].send({'s': c, 'sport': self.sport, 'eport': addr[1], 'cmd': NEW, 'master': False, 'payload': b'\0'})
                         else:
                             c.close()
-
-
 
         else:
             while self.running:
@@ -303,11 +302,11 @@ class Relay(Thread):
                         print('not ready')
                         for st in self.socks.values():
                             if st.is_listener:
+                                st.ready = False
+                            else:
                                 st.running = False
                                 st.kill()
                                 st.join()
-                            else:
-                                st.ready = False
 
                     elif data['cmd'] == READY:
                         print('ready')
@@ -343,195 +342,6 @@ class Relay(Thread):
 
         self.kill()
 
-'''
-class Server(Thread):
-    def __init__(self, port):
-        super(Server, self).__init__()
-        self.port = port
-        self.master_listener = None
-        self.listeners = {}
-        self.add_port('0.0.0.0', 5555, True)
-        self.running = True
-
-    def add_port(self, ip, port, master=False):
-        if port in self.listeners.keys():
-            return False
-
-        l = Listener(ip, port, master)
-
-        if master:
-            self.master_listener = l
-
-        l.start()
-        self.listeners[port] = l
-
-    def run(self):
-        while self.running:
-
-            # poll pipes for data
-            for l in self.listeners.values():
-                while l.pipe[0].poll():
-                    data = l.pipe[0].recv()
-
-                    # start listeners
-                    if data['cmd'] == READY:
-                        for l2 in self.listeners.values():
-                            if not l2.is_master:
-                                l2.start_socket()
-
-                    elif data['cmd'] == NOTREADY:
-                        for l2 in self.listeners.values():
-                            if not l2.is_master:
-                                l2.kill()
-
-                    elif data['cmd'] == OPEN or data['cmd'] == DATA or data['cmd'] == CLOSE:
-                        if l == self.master_listener:
-                            if data['sport'] in self.listeners.keys():
-                                self.listeners[data['sport']].pipe[0].send(data)
-                        else:
-                            self.master_listener.pipe[0].send(data)
-
-            time.sleep(0.05)
-
-        print('Server shutting down.')
-        for l in self.listeners.values():
-            l.running = False
-            l.join()
-
-class Listener(Thread):
-    def __init__(self, ip, port, master=False):
-        super(Listener, self).__init__()
-        self.ip = '0.0.0.0'
-        self.pipe = Pipe(True)
-        self.port = port
-        self.is_master = master
-        self.socks = {}
-        self.running = True
-
-    def kill(self):
-        for s in self.socks.values():
-            s.close()
-        self.socks = {}
-
-    def start_socket(self):
-        print('Starting socket:', self.port)
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.setblocking(0)
-            s.bind((self.ip, self.port))
-            s.listen(25)
-            self.socks[0] = s
-        except:
-            self.kill()
-            return False
-
-        return True
-
-    def stop_socket(self):
-        print('Stopping!', self.port)
-        if 0 in self.socks.keys():
-            self.socks[0].close()
-            del self.socks[0]
-
-    def close_socket(self, eport):
-        if eport in self.socks.keys():
-            self.socks[eport].close()
-            del self.socks[eport] 
-
-    def run(self):
-        if self.is_master:
-            self.start_socket()
-
-        print('Started:', self.port)
-        while self.running:
-            # poll for data / connections
-            ins = select.select(list(self.socks.values()), [], [], 0.01)[0]
-            for s in ins:
-                # new connection
-                if 0 in self.socks.keys() and s == self.socks[0]:
-                    co, addr = self.socks[0].accept()
-                    c = CustomSocket.copy(co)
-                    c.eport = addr[1]
-                    c.sport = s.getsockname()[1]
-                    co.close()
-                    print('Connection from: ', addr)
-
-                    # only allow 1 master connection
-                    if self.is_master:
-                        self.stop_socket()
-                        self.pipe[1].send({'sport': self.port, 'eport': addr[1], 'cmd': READY, 'payload': b'\0'})
-
-                    # send connect request
-                    else:
-                        self.pipe[1].send({'sport': self.port, 'eport': addr[1], 'cmd': OPEN, 'payload': b'\0'})
-
-                    self.socks[addr[1]] = c
-
-                elif s in self.socks.values():
-                    if self.is_master:
-                        try:
-                            data = recv_all(s, 4)
-                            if len(data) == 4:
-                                data = recv_all(s, struct.unpack('<I', data)[0])
-                        except:
-                            data = b''
-
-                    else:
-                        try:
-                            data = recv(s, 65535)
-                        except:
-                            data = b''
-
-                    if data == b'':
-                        # lost master
-                        if self.is_master:
-                            if not self.start_socket():
-                                continue
-
-                            self.pipe[1].send({'sport': s.sport, 'eport': s.eport, 'cmd': NOTREADY, 'payload': b'\0'})
-
-                        # lost client
-                        else:
-                            self.pipe[1].send({'sport': s.sport, 'eport': s.eport, 'cmd': CLOSE, 'payload': b'\0'})
-
-                        # remove port
-                        self.close_socket(s.eport)
-                        continue
-
-                    else:
-                        if self.is_master:
-                            data = struct.unpack('<BHH' + str(len(data[5:])) + 's', data)
-                            self.pipe[1].send({'cmd': data[0], 'sport': data[1], 'eport': data[2], 'payload': data[3]})
-                        else:
-                            self.pipe[1].send({'sport': s.sport, 'eport': s.eport, 'cmd': DATA, 'payload': data})
-
-            # send out data to clients
-            while self.pipe[1].poll():
-                data = self.pipe[1].recv()
-
-                if data['eport'] in self.socks.keys() or self.is_master:
-                    if (self.is_master):
-                        bin_data = struct.pack('<BHH' + str(len(data['payload'])) + 's', data['cmd'], data['sport'], data['eport'], data['payload'])
-                        bin_data = struct.pack('<I', len(bin_data)) + bin_data
-                        try:
-                            self.socks[list(self.socks.keys())[0]].send(bin_data)
-                        except:
-                            self.close_socket(0)
-                            self.pipe[1].send({'sport': s.sport, 'eport': s.eport, 'cmd': NOTREADY, 'payload': b'\0'})
-                            self.start_socket()
-                    else:
-                        if data['cmd'] == CLOSE:
-                            self.close_socket(data['eport'])
-                        elif data['cmd'] == DATA:
-                            try:
-                                self.socks[data['eport']].send(data['payload'])
-                            except:
-                                self.close_socket(data['eport'])
-
-        # close all connections
-        self.kill()
-'''
 
 def main():
     global running
